ffbd6532456f606237ab15a0238c1a9571560c81
Alec Hajinoff <hajinoff@gmail.com>
2024-12-18 17:58:42 +0000

    18.12.24, 17.58

--- login_capture.php
+++ login_capture.php
@@ -2,17 +2,13 @@
 session_start();
 $pdo = new PDO('mysql:host=localhost;dbname=climate_bind', 'root', ''); 
 $email = $_POST['email'];
-$password = $_POST['password'];
-var_dump($email); 
-var_dump($password); 
-var_dump($_POST);
-print_r($_POST); 
+$password = $_POST['password']; 
 $stmt = $pdo->prepare('SELECT * FROM user_data WHERE email = ?');
 $stmt->execute([$email]);
 $user = $stmt->fetch();
 if ($user && $password) {
     $_SESSION['user_id'] = $user['id'];
-    header('Location: localhost:8001/Climate_Bind_Development/login_capture.php'); 
+    header('Location: http://localhost:8001/Climate_Bind_Development/login_capture.php'); 
 } else {
     echo 'Invalid email or password.';
 }
