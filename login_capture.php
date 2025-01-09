948a58a65779ca8e80b71a5f31d74f34f1e2cea0
Alec Hajinoff <hajinoff@gmail.com>
2025-01-09 17:30:54 +0000

    9.1.25, 17.30

--- login_capture.php
+++ login_capture.php
@@ -1,22 +1,27 @@
 <?php
 if (isset($_POST['email'], $_POST['password'])) {
+    echo "1";
     session_start();
     $email = trim($_POST['email']);
     $password = $_POST['password'];
     try {
+        echo "2";
         $pdo = new PDO('mysql:host=localhost;dbname=climate_bind', 'root', '', [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]);
-        $stmt = $pdo->prepare('SELECT * FROM user_data WHERE email = ?');
+        echo "3";
+        $stmt = $pdo->prepare('SELECT * FROM users WHERE email = ?');
         $stmt->execute([$email]);
         $user = $stmt->fetch();
+        echo "3a";
         //var_dump($user);
         //var_dump($password);
         //var_dump($user['password']);
         //var_dump($user['email']);
         //var_dump(password_verify($password, $user['password']));
         if ($user/*['email']*/ && password_verify($password, $user['password'])) {
+            echo "4";
             $_SESSION["user_id"] = $user["email"];
             //$response = ['status' => 'success', 'data' => 'Login successful'];
             //file_put_contents('log.txt', json_encode($response) . PHP_EOL, FILE_APPEND);
