e5aeb07ec70fc251d944cb52f30b0a5c6eb2a91e
Alec Hajinoff <hajinoff@gmail.com>
2025-01-18 17:48:53 +0000

    18.1.25, 17.48

--- React/src/App.js
+++ React/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Soon!
+          Learn React Soon! Test!
         </a>
       </header>
     </div>
