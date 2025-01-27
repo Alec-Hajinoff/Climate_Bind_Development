4820347c46271cc9b4715db2fa240d99c28523f9
Alec Hajinoff <hajinoff@gmail.com>
2025-01-27 07:29:15 +0000

    27.1.25, 7.29

--- React/src/AccountPage.js
+++ React/src/AccountPage.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import blue from './blue.svg';
 import './AccountPage.css';
+import AccountDataCapture from './AccountDataCapture.js';
 
 function AccountPage() {
   return (
     <div className="container text-center">
       <div className="row">
-        <div className="col-12 col-md-8">
-          <p>Thank you for registering! This is your account page.</p>
+        <div className="col-12">
+          <p>Thank you for logging in and welcome to your account page! Please fill in the form below to start your insurance policy. You can save your progress and fill in the form in several sittings.</p>
+          <AccountDataCapture />
         </div>
       </div>
     </div>
