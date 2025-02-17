43280bad59c18f57043c5475d243f8362d577357
Alec Hajinoff <hajinoff@gmail.com>
2025-02-17 17:51:29 +0000

    17.2.25, 17.51

--- React/src/App.test.js
+++ React/src/App.test.js
@@ -1,14 +1,9 @@
 import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
 import "@testing-library/jest-dom";
 import App from "./App";
 
 test("renders learn react link", () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>
-  );
+  render(<App />);
   const linkElement = screen.getByText(/New user/i);
   expect(linkElement).toBeInTheDocument();
 });
--- React/src/UserRegistration.test.js
+++ React/src/UserRegistration.test.js
@@ -1,19 +1,16 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
-import { useNavigate } from "react-router-dom";
-jest.mock("react-router-dom", () => ({
-  ...jest.requireActual("react-router-dom"),
-  useNavigate: jest.fn(),
-}));
-import UserRegistration from "./UserRegistration";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import UserRegistration from "./UserRegistration"; 
 
 describe("UserRegistration Component", () => {
-  test("updates first name input field correctly", () => {
-    
-    render(<UserRegistration />);
-    
+  it("should render the first name input with the correct placeholder", () => {
+    render(
+      <BrowserRouter>
+        <UserRegistration />
+      </BrowserRouter>
+    );
     const firstNameInput = screen.getByPlaceholderText("Your first name");
-    
     expect(firstNameInput).toBeInTheDocument();
   });
 });
