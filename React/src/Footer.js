5c432eaa83998719866a791d3217609ef01081c5
MirrorInstinct <jarolvaz97@gmail.com>
2025-03-05 10:35:14 -0500

    added copyright to footer, updates yearly

--- React/src/Footer.js
+++ React/src/Footer.js
@@ -3,6 +3,8 @@ import blue from "./blue.svg";
 import "./Footer.css";
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="container text-center">
       <br />
@@ -11,9 +13,10 @@ function Footer() {
           <em>
             Company address: 4 Bridge Gate, London, N21 2AH, United Kingdom.
             Email address:<span> </span>
-            <a href="mailto:team@climatebind.com">team@climatebind.com</a>
+            <a href="team@climatebind.com">team@climatebind.com</a>
           </em>
         </p>
+        <p>&copy; 2024 - {currentYear} Climate Bind. All rights reserved.</p>
       </div>
     </div>
   );
