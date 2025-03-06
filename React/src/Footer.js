19bc6f58a15762ba1f2c9445ca0318d93356daab
Alec Hajinoff <hajinoff@gmail.com>
2025-03-06 05:53:08 +0000

    6.3.25, 5.53

--- React/src/Footer.js
+++ React/src/Footer.js
@@ -11,6 +11,32 @@ function Footer() {
       <div className="row">
         <p className="footer">
           <em>
+            &copy; Copyright 2024 - {currentYear}. All rights reserved.
+            Company address: 4 Bridge Gate, London, N21 2AH, United Kingdom.
+            Email address:<span> </span>
+            <a href="team@climatebind.com">team@climatebind.com</a>
+          </em>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
+/*
+import React from "react";
+import blue from "./blue.svg";
+import "./Footer.css";
+
+function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className="container text-center">
+      <br />
+      <div className="row">
+        <p className="footer">
+          <em>
             Company address: 4 Bridge Gate, London, N21 2AH, United Kingdom.
             Email address:<span> </span>
             <a href="team@climatebind.com">team@climatebind.com</a>
@@ -23,3 +49,4 @@ function Footer() {
 }
 
 export default Footer;
+*/
