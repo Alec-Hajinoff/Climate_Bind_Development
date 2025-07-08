d176144ef020e618270c81e6dea25ebadca26f3d
Alec Hajinoff <hajinoff@gmail.com>
2025-07-08 17:58:39 +0100

    8.7.25, 17.58

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@matterlabs/hardhat-zksync-verify");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    zkSyncSepolia: {
+      url: "https://sepolia.era.zksync.dev",
+      chainId: 300,
+      zksync: true,
+    },
+  },
+  etherscan: {
+    customChains: [
+      {
+        network: "zkSyncSepolia",
+        chainId: 300,
+        urls: {
+          apiURL: "https://sepolia.explorer.zksync.io/api",
+          browserURL: "https://sepolia.explorer.zksync.io",
+        },
+      },
+    ],
+  },
 };
